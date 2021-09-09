e7e75479b7d80eef11eabb3584e38f4a364c921f
Matija Karapetrić <matija@epicentar.net>
2021-09-09 19:17:56 +0200

    commit

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const h1 = document.createElement('h1');
-h1.innerHTML = "Hello world!";
-document.body.appendChild(h1);
+const helloWorld = require("./helloWorld");
+
+helloWorld.printHelloWorld();
